7ff2b6aea564fa33fdff656d66ffb117eec28d3b
Kristjan Broder Lund <kristjan.1234@gmail.com>
2012-10-21 23:36:21 +0000

    Minor changes to example/read.js

--- example/read.js
+++ example/read.js
@@ -1,7 +1,8 @@
-var TFTP = require('..');
+var TFTP = require('..'),
+	fs = require('fs');
 
 
-// Initialize the ftp client
+// Initialize the tftp client
 var client = new TFTP(69, '78.47.194.67');
 
 client.read('1.txt', function (err, data) {
@@ -11,6 +12,8 @@ client.read('1.txt', function (err, data) {
 		return;
 	}
 
-	console.log('Got data:');
-	console.log(data.toString());
+	console.log('Got data (%d bytes). First 100 bytes:', data.length);
+	console.log(data.toString('utf8', 0, 100));
+
+	fs.writeFileSync('1.txt', data);
 });
