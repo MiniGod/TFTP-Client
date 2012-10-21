011784e69d952cec867d508e3b73a4c378e080eb
Kristjan Broder Lund <kristjan.1234@gmail.com>
2012-10-21 22:35:49 +0000

    Now using a queue, and read works

--- example/read.js
+++ example/read.js
@@ -4,4 +4,13 @@ var TFTP = require('..');
 // Initialize the ftp client
 var client = new TFTP(69, '78.47.194.67');
 
-client.getFile('1.txt');
+client.read('1.txt', function (err, data) {
+	if (err) {
+		console.error('ERROR:');
+		console.error(err);
+		return;
+	}
+
+	console.log('Got data:');
+	console.log(data.toString());
+});
